489ef74f829d616fa85324bec5c58a4d539b5556
Luis Pablo Gallo <luispablo.gallo@gmail.com>
2022-12-27 14:59:11 -0300

    Add logo to top menu

--- app/ui/Application.js
+++ app/ui/Application.js
@@ -12,7 +12,8 @@ const Application = function Application () {
         <nav className="navbar fixed-top navbar-expand navbar-dark bg-primary">
           <div className="container-fluid">
             <Link to="/" className="navbar-brand">
-              Camp Gladiator Mars
+              <img src="img/logo.png" alt="logo" className="d-inline-block align-text-top" />&nbsp;
+              Camp Gladiator (Mars)
             </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
